d55fcec7c84c35fb65a1b170929b5cd4abb9c4b7
Ahtisham Muzaffar <Ehtasham-Muzaffar>
2022-04-01 13:23:19 +0500

    first version

---
+++ app.js
@@ -0,0 +1,48 @@
+var game=document.getElementById('game')
+var redcar = document.getElementById("redcar")
+var bluecar=document.getElementById("bluecar")
+var score=document.getElementById("score")
+var count=0
+window.addEventListener("keydown",function(e){
+    
+    if(e.keyCode == "39"){
+       
+var racecar=parseInt(this.window.getComputedStyle(redcar).getPropertyValue("left"))
+if(racecar < 260){
+    redcar.style.left= (racecar + 130) + "px"
+    }}
+
+    if(e.keyCode == "37"){
+        var racecar=parseInt(this.window.getComputedStyle(redcar).getPropertyValue("left"))
+        if(racecar > 0){
+            redcar.style.left= (racecar - 130) + "px"
+            }}
+})
+
+bluecar.addEventListener("animationiteration",()=>{
+    count++
+    var random= ((Math.floor(Math.random()*3))*130)
+    bluecar.style.left=random + "px"
+
+    score.innerHTML = `score: ${count}`;
+
+})
+setInterval(() => {
+    
+        var bluecartop=parseInt(window.getComputedStyle(bluecar).getPropertyValue('top'))
+        var blueleft=parseInt(window.getComputedStyle(bluecar).getPropertyValue("left"))
+        var redcarleft=parseInt(window.getComputedStyle(redcar).getPropertyValue('left'))
+    
+        if((blueleft==redcarleft) && (bluecartop > 270) && (bluecartop < 480)){
+        game.style.display="none"
+        
+        count=0
+
+        }
+
+    
+}, 10);
+
+setTimeout(()=>{
+bluecar.style.animationDuration= `${0.7}s`
+},180000)
